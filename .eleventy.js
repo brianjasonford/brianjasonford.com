2893c897e6d63147409d2cea670ac21ab56a3a6f
brianjasonford <61121683+brianjasonford@users.noreply.github.com>
2025-02-26 19:23:14 -0500

    Update .eleventy.js
    Added passthroughs for /css and /images. Added filters for postDate, filterTagList, and sortTags

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,34 @@
-module.exports = (config) => {
-    return {
-        dir: {
-            input: 'src',
-            output: 'public',
-        }
-    }
-}
+const { DateTime } = require("luxon");
+
+module.exports = (eleventyConfig) => {
+    // Passthroughs
+    eleventyConfig.addPassthroughCopy('./src/css/');
+	eleventyConfig.addPassthroughCopy('./src/images/');
+
+	// Filters
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLLL yyyy");
+	});
+
+	// This filter removes "all" and "posts" from the tag list
+	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+		return (tags || []).filter(tag => ["all", "posts"].indexOf(tag) === -1);
+	});
+
+	// This filter puts the tags in alphabetical order
+	eleventyConfig.addFilter("sortTags", function(tags) {
+		if (!Array.isArray(tags)) return tags;
+		return tags
+			.filter(tag => typeof tag === "string") // Ensure only strings are processsed 
+			.sort((a, b) => a.localeCompare(b));
+	});
+
+	
+
+	return {
+		dir: {
+			input: 'src',
+			output: 'public',
+		},
+	};
+};
